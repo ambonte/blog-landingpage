0eecbbd2bccecb6b8748f1667c94eab70302a607
Bonte Emma BANA <bbonteemma@gmail.com>
2021-06-23 15:39:11 +0200

    adding changes

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+const hamBurger = document.querySelector(".hamburger");
+  hamBurger.addEventListener("click", function () {
+    document.querySelector(".nav-links").classList.toggle("hide-menu");
+  });
+
